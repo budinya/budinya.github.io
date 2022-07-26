a9d3cdff2e868c4f857f31eff4df2521da1bd699
budinya <budin@paaa.cn>
2022-07-26 21:24:22 +0800

    new

---
+++ js/tx.js
@@ -0,0 +1,7 @@
+	function mouseOver(){
+		document.getElementById("mainimg").src="src/tx.png";//鼠标悬浮时的旋转图标
+	}
+	function mouseOut()
+	{
+		document.getElementById('mainimg').src ="src/tx2.png"//默认的旋转图标
+	}
